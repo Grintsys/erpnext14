b0bab342d3063eed0cbc73654090300ad44f29b9
VicenteDavidG <Vicentedavidg@gmail.com>
2025-06-26 04:15:46 +0000

    Mejorando rendimiento de Reporte General de Ventas y costos

--- erpnext/selling/report/general_de_ventas_con_costo_y_utilidad/general_de_ventas_con_costo_y_utilidad.py
+++ erpnext/selling/report/general_de_ventas_con_costo_y_utilidad/general_de_ventas_con_costo_y_utilidad.py
@@ -6,165 +6,92 @@ import frappe
 from frappe.utils import flt
 from frappe import _, msgprint
 
-
 def execute(filters=None):
-	if not filters: filters = {}
+    if not filters: filters = {}
 
-	columns = [
-		{
-   			"fieldname": "date",
-  			"fieldtype": "Date",
-  			"label": "Fecha",
-			"width": 100
-  		},
-		{
-			"fieldname": "rtn",
-   			"fieldtype": "Data",
-   			"label": "RTN",
-			"width": 120
-		},
-		{
-			"fieldname": "name",
-   			"fieldtype": "Data",
-   			"label": "Nombre",
-			"width": 140
-		},
-		{
-			"fieldname": "document",
-   			"fieldtype": "Link",
-			"options": "Sales Invoice",
-   			"label": "Documento",
-			"width": 140
-		},
-		{
-			"fieldname": "type_document",
-   			"fieldtype": "Data",
-   			"label": "Tipo de documento",
-			"width": 140
-		},
-		{
-   			"fieldname": "total_exempt",
-  			"fieldtype": "Currency",
-  			"label": "Total Exento",
-			"width": 110
-  		},
-		{
-			"fieldname": "base_isv_15%",
-   			"fieldtype": "Currency",
-   			"label": "Base ISV 15%",
-			"width": 110
-		},
-		{
-			"fieldname": "isv_15%",
-   			"fieldtype": "Currency",
-   			"label": "ISV 15%",
-			"width": 110
-		}		,
-		{
-			"fieldname": "base_isv_18%",
-   			"fieldtype": "Currency",
-   			"label": "Base ISV 18%",
-			"width": 110
-		},
-		{
-			"fieldname": "isv_18%",
-   			"fieldtype": "Currency",
-   			"label": "ISV 18%",
-			"width": 110
-		},
-		{
-			"fieldname": "discount_amount",
-   			"fieldtype": "Currency",
-   			"label": "Descuento",
-			"width": 110
-		},
-		{
-			"fieldname": "total",
-   			"fieldtype": "Currency",
-   			"label": "Total",
-			"width": 110
-		},		
-		{
-			"fieldname": "total_final",
-   			"fieldtype": "Currency",
-   			"label": "Total Final",
-			"width": 110
-		},		
-		{
-			"fieldname": "cost",
-   			"fieldtype": "Currency",
-   			"label": "Costo",
-			"width": 110
-		},		
-		{
-			"fieldname": "utility",
-   			"fieldtype": "Currency",
-   			"label": "Utilidad",
-			"width": 110
-		},		
-		{
-			"fieldname": "utility_percentage",
-   			"fieldtype": "Currency",
-   			"label": "% Utilidad",
-			"width": 110
-		}
-	]
+    columns = [
+        { "fieldname": "date", "fieldtype": "Date", "label": "Fecha", "width": 100 },
+        { "fieldname": "rtn", "fieldtype": "Data", "label": "RTN", "width": 120 },
+        { "fieldname": "name", "fieldtype": "Data", "label": "Nombre", "width": 140 },
+        { "fieldname": "document", "fieldtype": "Link", "options": "Sales Invoice", "label": "Documento", "width": 140 },
+        { "fieldname": "type_document", "fieldtype": "Data", "label": "Tipo de documento", "width": 140 },
+        { "fieldname": "total_exempt", "fieldtype": "Currency", "label": "Total Exento", "width": 110 },
+        { "fieldname": "base_isv_15%", "fieldtype": "Currency", "label": "Base ISV 15%", "width": 110 },
+        { "fieldname": "isv_15%", "fieldtype": "Currency", "label": "ISV 15%", "width": 110 },
+        { "fieldname": "base_isv_18%", "fieldtype": "Currency", "label": "Base ISV 18%", "width": 110 },
+        { "fieldname": "isv_18%", "fieldtype": "Currency", "label": "ISV 18%", "width": 110 },
+        { "fieldname": "discount_amount", "fieldtype": "Currency", "label": "Descuento", "width": 110 },
+        { "fieldname": "total", "fieldtype": "Currency", "label": "Total", "width": 110 },
+        { "fieldname": "total_final", "fieldtype": "Currency", "label": "Total Final", "width": 110 },
+        { "fieldname": "gross_amount", "fieldtype": "Currency", "label": "Monto Bruto", "width": 110 },
+        { "fieldname": "cost", "fieldtype": "Currency", "label": "Costo", "width": 110 },
+        { "fieldname": "utility", "fieldtype": "Currency", "label": "Utilidad", "width": 110 },
+        { "fieldname": "utility_percentage", "fieldtype": "Currency", "label": "% Utilidad", "width": 110 }
+    ]
 
-	data = []
+    data = []
+    customer_tax_ids = {}
+    conditions = return_filters(filters)
 
-	conditions = return_filters(filters)
-	sales_invoice = frappe.get_all("Sales Invoice", ["*"], filters = conditions, order_by='name')
-	
-	for sales in sales_invoice:
-		customer = frappe.get_doc("Customer", sales.customer)
-		type_document = "Factura de venta"
-		if(sales.is_return): type_document = "Devolución"
+    sales_invoice = frappe.get_all("Sales Invoice", [
+        "name", "customer", "posting_date", "exempt_amount",
+        "taxed_amount_15", "isv_15", "taxed_amount_18", "isv_18",
+        "discount_amount", "rounded_total", "grand_total", "is_return"
+    ], filters=conditions, order_by='name')
 
-		cost = 0
-		utility = 0
-		utility_percentage = 0
+    for sales in sales_invoice:
+        if sales.customer not in customer_tax_ids:
+            customer_tax_ids[sales.customer] = frappe.db.get_value("Customer", sales.customer, "tax_id")
+        tax_id = customer_tax_ids[sales.customer]
 
-		sales_invoice_items = frappe.get_all("Sales Invoice Item", ["*"], filters = {"parent": sales.name})
+        type_document = "Factura de venta"
+        if sales.is_return:
+            type_document = "Devolución"
 
-		for item in sales_invoice_items:
-			cost += item.incoming_rate
-			utility += item.rate - item.incoming_rate
-		
-		if cost > 0:
-			utility_percentage = (utility / cost) * 100
+        cost = 0
+        utility = 0
+        utility_percentage = 0
 
-		row = [
-			sales.posting_date,
-			customer.tax_id,
-			sales.customer,
-			sales.name,
-			type_document,
-			sales.exempt_amount,
-			sales.taxed_amount_15,
-			sales.isv_15,
-			sales.taxed_amount_18,
-			sales.isv_18,
-			sales.discount_amount,
-			sales.rounded_total,
-			sales.grand_total,
-			cost,
-			utility,
-			utility_percentage
-		]
-		data.append(row)
+        items = frappe.get_all("Sales Invoice Item", [
+            "incoming_rate", "rate"
+        ], filters={"parent": sales.name})
 
-	return columns, data
+        for item in items:
+            cost += flt(item.incoming_rate)
+            utility += flt(item.rate) - flt(item.incoming_rate)
+
+        if cost > 0:
+            utility_percentage = (utility / cost) * 100
+
+        gross_amount = flt(sales.exempt_amount) + flt(sales.taxed_amount_15) + flt(sales.taxed_amount_18) - flt(sales.discount_amount)
+
+        row = [
+            sales.posting_date,
+            tax_id,
+            sales.customer,
+            sales.name,
+            type_document,
+            sales.exempt_amount,
+            sales.taxed_amount_15,
+            sales.isv_15,
+            sales.taxed_amount_18,
+            sales.isv_18,
+            sales.discount_amount,
+            sales.rounded_total,
+            sales.grand_total,
+            gross_amount,
+            cost,
+            utility,
+            utility_percentage
+        ]
+        data.append(row)
+
+    return columns, data
 
 def return_filters(filters):
-	conditions = ''
-
-	conditions += "{"
-	if filters.get("from_date") and filters.get("to_date"):
-		conditions += '"posting_date": ["between", ["{}", "{}"]]'.format(
-			filters["from_date"], filters["to_date"]
-		)
-
-	if filters.get("company"): conditions += ', "company": "{}"'.format(filters.get("company"))
-	conditions += '}'
-
-	return conditions
+    conditions = {}
+    if filters.get("from_date") and filters.get("to_date"):
+        conditions["posting_date"] = ["between", [filters["from_date"], filters["to_date"]]]
+    if filters.get("company"):
+        conditions["company"] = filters.get("company"]
+    return conditions
