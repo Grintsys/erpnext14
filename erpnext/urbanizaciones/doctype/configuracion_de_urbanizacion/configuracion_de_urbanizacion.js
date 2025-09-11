38704175c5c4d83bafd1a579d7c3de50aed2d2ac
VicenteDavidG <Vicentedavidg@gmail.com>
2025-09-11 05:50:39 +0000

    ‘10.09.02’

---
+++ erpnext/urbanizaciones/doctype/configuracion_de_urbanizacion/configuracion_de_urbanizacion.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Configuracion de Urbanizacion', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ erpnext/urbanizaciones/doctype/urbanizaciones/urbanizaciones.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Urbanizaciones', {
+	// refresh: function(frm) {
+
+	// }
+});
