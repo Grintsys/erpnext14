b851cf6259542b473144839e7b8ee5239be3f083
Eliazar Melendez <eliazar.melendez@gmail.com>
2025-09-17 03:56:15 +0000

    WIP: Agregado configuracion de financiamiento y calculos basicos de fecha de vencimiento y saldo actual

--- erpnext/urbanizaciones/doctype/financiamientos/financiamientos.js
+++ erpnext/urbanizaciones/doctype/financiamientos/financiamientos.js
@@ -2,6 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Financiamientos', {
+	//refresh: function(frm) {
 	onload: function(frm)
 	{
 		// Get a single value from a doctype and set the value into the current doctype
@@ -11,16 +12,18 @@ frappe.ui.form.on('Financiamientos', {
 		// 	});
 
 		// Get multiple value from a doctype and set the values into the current doctype
-		frappe.db.get_doc('Configuracion de Urbanizacion')
-			.then(config => {
-				frm.set_value('interes_anual', config.interes_anual);
-				frm.set_value('mora_diaria', config.mora_diaria);
-				frm.set_value('vencimiento_cuota', config.vencimiento_cuota);
-			});
+		// frappe.db.get_doc('Configuracion de Urbanizacion')
+		// 	.then(config => {
+		// 		frm.set_value('interes_anual', config.interes_anual);
+		// 		frm.set_value('mora_diaria', config.mora_diaria);
+		// 		frm.set_value('vencimiento_cuota', config.vencimiento_cuota);
+		// 	});
 
 		// Calculate capital_financiado on load if fields already have values
 		calculate_capital_financiero(frm);
 		calculate_cuota_estimada(frm);
+		calculate_proxima_fecha(frm);
+		calculate_saldo_actual(frm);
 	},
 
 	monto_contrato: function(frm)
@@ -43,6 +46,21 @@ frappe.ui.form.on('Financiamientos', {
 	plazo_meses: function(frm)
 	{
 		calculate_cuota_estimada(frm);
+	},
+
+	fecha_inicio: function(frm)
+	{
+		calculate_proxima_fecha(frm);
+	},
+
+	dia_vencimiento_cuota: function(frm)
+	{
+		calculate_proxima_fecha(frm);
+	},
+
+	capital_financiado: function(frm)
+	{
+		calculate_saldo_actual(frm);
 	}
 });
 
@@ -58,8 +76,6 @@ function calculate_capital_financiero(frm)
 // Function to calculate cuota_estimada
 function calculate_cuota_estimada(frm)
 {
-	console.log("Enter the method");
-	
 	let interes_anual = frm.doc.interes_anual || 0;
 	let capital = frm.doc.capital_financiado || 0;
 	let cuotas = frm.doc.plazo_meses || 0;
@@ -81,4 +97,32 @@ function calculate_cuota_estimada(frm)
 		console.log("Enter else");
 		frm.set_value('cuota_estimada', 0);
 	}
+}
+
+function calculate_proxima_fecha(frm)
+{
+	//Todo: si ya existe el plan de pago, mostrar la fecha de vencimiento de la cuota correspondiente
+	
+	const fecha_inicio = frm.doc.fecha_inicio;
+	const dia_vencimiento = frm.doc.dia_vencimiento_cuota;
+
+	if (fecha_inicio && dia_vencimiento)
+	{
+		let fecha = frappe.datetime.str_to_obj(fecha_inicio);
+		
+		fecha.setMonth(fecha.getMonth() + 1);
+		fecha.setDate(dia_vencimiento);
+
+		frm.set_value('fecha_vencimiento_cuota', frappe.datetime.obj_to_str(fecha));
+	}
+	else
+	{
+		frm.set_value('fecha_vencimiento_cuota', null);
+	}
+}
+
+function calculate_saldo_actual(frm)
+{
+	// Todo: Hacer el calculo correspondiente al capital a medida se pagan las cuotas
+	frm.set_value('saldo_actual', frm.doc.capital_financiado);
 }
