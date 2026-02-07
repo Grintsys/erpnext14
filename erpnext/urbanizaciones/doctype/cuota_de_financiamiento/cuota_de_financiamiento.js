e0b10979a8ab2872fd9da47dbfdea38c710382d6
Eliazar Melendez <eliazar.melendez@gmail.com>
2026-02-07 16:05:51 +0000

    Cambios en tablas para el pago de cuota

--- erpnext/urbanizaciones/doctype/cuota_de_financiamiento/cuota_de_financiamiento.js
+++ erpnext/urbanizaciones/doctype/cuota_de_financiamiento/cuota_de_financiamiento.js
@@ -4,5 +4,9 @@
 frappe.ui.form.on('Cuota de financiamiento', {
 	// refresh: function(frm) {
 
-	// }
+	// },
+	pagar_btn: function (frm) {
+		// Logica para el boton Pagar
+		console.log("Boton Pagar presionado");
+	}
 });
