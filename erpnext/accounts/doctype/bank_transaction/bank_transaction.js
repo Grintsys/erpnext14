62aa8e4bcaa19c05ef42000d8185b395370dec46
VicenteDavidG <Vicentedavidg@gmail.com>
2026-02-24 00:30:00 +0000

    Cambios en transaccion de banco

--- erpnext/accounts/doctype/bank_transaction/bank_transaction.js
+++ erpnext/accounts/doctype/bank_transaction/bank_transaction.js
@@ -13,6 +13,7 @@ frappe.ui.form.on("Bank Transaction", {
 		});
 	},
 	refresh(frm) {
+		frm.events.calculate_totals(frm);
 		if (!frm.is_dirty() && frm.doc.payment_entries.length > 0) {
 			frm.add_custom_button(__("Unreconcile Transaction"), () => {
 				frm.call("remove_payment_entries").then(() => frm.refresh());
@@ -75,6 +76,135 @@ frappe.ui.form.on("Bank Transaction", {
 	},
 	bank_account: function (frm) {
 		set_bank_statement_filter(frm);
+		frm.events.sync_bank_account(frm);
+	},
+
+	deposit: function (frm) {
+		frm.events.update_first_row_amounts(frm);
+	},
+
+	withdrawal: function (frm) {
+		frm.events.update_first_row_amounts(frm);
+	},
+
+	sync_bank_account: function (frm) {
+		if (frm.doc.bank_account) {
+			frappe.db.get_value('Bank Account', frm.doc.bank_account, 'account', function (r) {
+				if (r && r.account) {
+					if (!frm.doc.custom_journal_entries || frm.doc.custom_journal_entries.length === 0) {
+						frm.add_child('custom_journal_entries');
+					}
+					let first_row = frm.doc.custom_journal_entries[0];
+					frappe.model.set_value(first_row.doctype, first_row.name, 'account', r.account).then(() => {
+						frm.events.update_first_row_amounts(frm);
+					});
+				}
+			});
+		}
+	},
+
+	update_first_row_amounts: function (frm) {
+		if (frm.doc.custom_journal_entries && frm.doc.custom_journal_entries.length > 0) {
+			let first_row = frm.doc.custom_journal_entries[0];
+			let deposit = flt(frm.doc.deposit);
+			let withdrawal = flt(frm.doc.withdrawal);
+
+			if (deposit > 0) {
+				frappe.model.set_value(first_row.doctype, first_row.name, 'debit_in_account_currency', deposit);
+				frappe.model.set_value(first_row.doctype, first_row.name, 'credit_in_account_currency', 0);
+			} else if (withdrawal > 0) {
+				frappe.model.set_value(first_row.doctype, first_row.name, 'credit_in_account_currency', withdrawal);
+				frappe.model.set_value(first_row.doctype, first_row.name, 'debit_in_account_currency', 0);
+			} else {
+				frappe.model.set_value(first_row.doctype, first_row.name, 'debit_in_account_currency', 0);
+				frappe.model.set_value(first_row.doctype, first_row.name, 'credit_in_account_currency', 0);
+			}
+			frm.events.calculate_totals(frm);
+		}
+	},
+
+	calculate_totals: function (frm) {
+		if (!frm.doc.custom_journal_entries || frm.doc.custom_journal_entries.length === 0) {
+			frm.set_intro("");
+			return;
+		}
+
+		let total_debit = 0;
+		let total_credit = 0;
+
+		frm.doc.custom_journal_entries.forEach(row => {
+			total_debit += flt(row.debit_in_account_currency);
+			total_credit += flt(row.credit_in_account_currency);
+		});
+
+		let diff = Math.abs(total_debit - total_credit);
+		let indicator = diff === 0 ? "green" : "red";
+
+		let msg = `<b>Asiento Contable</b> - Total Debe: ${format_currency(total_debit)} | Total Haber: ${format_currency(total_credit)} | Diferencia: ${format_currency(diff)}`;
+
+		frm.set_intro(msg, indicator);
+	},
+
+	before_submit: function (frm) {
+		if (!frm.doc.custom_journal_entries || frm.doc.custom_journal_entries.length === 0) {
+			frappe.throw(__('La tabla de Asientos Contables debe tener al menos una fila.'));
+		}
+
+		let first_row = frm.doc.custom_journal_entries[0];
+		let total_debit = 0;
+		let total_credit = 0;
+
+		frm.doc.custom_journal_entries.forEach(row => {
+			total_debit += flt(row.debit_in_account_currency);
+			total_credit += flt(row.credit_in_account_currency);
+		});
+
+		if (flt(total_debit) !== flt(total_credit)) {
+			frappe.throw(__('No se puede someter: Los totales del Asiento Contable en la tabla no están cuadrados. Diferencia: {0}', [format_currency(Math.abs(total_debit - total_credit))]));
+		}
+
+		if (flt(frm.doc.deposit) > 0 && flt(total_debit) !== flt(frm.doc.deposit)) {
+			frappe.throw(__('No se puede someter: El Total Debe ({0}) debe coincidir con el monto del Depósito ({1}).',
+				[format_currency(total_debit), format_currency(frm.doc.deposit)]));
+		}
+		if (flt(frm.doc.withdrawal) > 0 && flt(total_credit) !== flt(frm.doc.withdrawal)) {
+			frappe.throw(__('No se puede someter: El Total Haber ({0}) debe coincidir con el monto del Retiro ({1}).',
+				[format_currency(total_credit), format_currency(frm.doc.withdrawal)]));
+		}
+
+		if (!frm.doc.bank_account) {
+			frappe.throw(__('Seleccione una Cuenta Bancaria.'));
+		}
+
+		return new Promise((resolve, reject) => {
+			frappe.db.get_value('Bank Account', frm.doc.bank_account, 'account', function (r) {
+				if (r && r.account) {
+					if (first_row.account !== r.account) {
+						frappe.msgprint({
+							message: __('No se puede someter: La primera línea debe corresponder a la cuenta contable del banco ({0}).', [r.account]),
+							indicator: 'red'
+						});
+						reject();
+					} else {
+						resolve();
+					}
+				} else {
+					frappe.msgprint({
+						message: __('No se encontró cuenta contable para el Banco seleccionado.'),
+						indicator: 'red'
+					});
+					reject();
+				}
+			});
+		});
+	},
+
+	custom_journal_entries_add: function (frm) {
+		frm.events.calculate_totals(frm);
+	},
+
+	custom_journal_entries_remove: function (frm) {
+		frm.events.calculate_totals(frm);
 	},
 
 	setup: function (frm) {
@@ -97,6 +227,18 @@ frappe.ui.form.on("Bank Transaction Payments", {
 	payment_entries_remove: function (frm, cdt, cdn) {
 		update_clearance_date(frm, cdt, cdn);
 	},
+});
+
+frappe.ui.form.on("Journal Entry Account", {
+	debit_in_account_currency: function (frm, cdt, cdn) {
+		frm.events.calculate_totals(frm);
+	},
+	credit_in_account_currency: function (frm, cdt, cdn) {
+		frm.events.calculate_totals(frm);
+	},
+	account: function (frm, cdt, cdn) {
+		frm.events.calculate_totals(frm);
+	}
 });
 
 const update_clearance_date = (frm, cdt, cdn) => {
