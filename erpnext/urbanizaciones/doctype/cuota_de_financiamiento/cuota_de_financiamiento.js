cc1511e996ceb345916e49d9ac9584ad1c24e03d
Eliazar Melendez <eliazar.melendez@gmail.com>
2026-03-10 19:19:16 +0000

    Se restauro la opcion de pagar desde la pantalla de cuotas

--- erpnext/urbanizaciones/doctype/cuota_de_financiamiento/cuota_de_financiamiento.js
+++ erpnext/urbanizaciones/doctype/cuota_de_financiamiento/cuota_de_financiamiento.js
@@ -7,6 +7,7 @@ frappe.ui.form.on('Cuota de financiamiento', {
 	// },
 	pagar_btn: function (frm) {
 		// Logica para el boton Pagar
-		console.log("Boton Pagar presionado");
+		// console.log("Boton Pagar presionado");
+		frappe.msgprint("Hello from Frappe!");
 	}
 });
