2ea48e2c5bdad41c8be2e3b20155603814d1bdc3
Eliazar Melendez <eliazar.melendez@gmail.com>
2026-03-29 18:18:52 +0000

    Agregada busqueda de finanziamientos por cliente

--- erpnext/urbanizaciones/doctype/generar_factura/generar_factura.js
+++ erpnext/urbanizaciones/doctype/generar_factura/generar_factura.js
@@ -2,7 +2,31 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Generar Factura', {
-	// refresh: function(frm) {
-
-	// }
+    customer: function(frm) {
+        // Limpia el campo de financiamiento cuando cambia el cliente
+        frm.set_value('financiamiento', '');
+        
+        // Actualiza las opciones del campo financiamiento
+        if (frm.doc.customer) {
+            frappe.call({
+                method: 'erpnext.urbanizaciones.doctype.generar_factura.generar_factura.get_financiamientos',
+                args: {
+                    customer: frm.doc.customer
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        // Actualiza el filtro del campo
+                        frm.fields_dict.financiamiento.get_query = function() {
+                            return {
+                                filters: {
+                                    "customer": frm.doc.customer,
+                                    "status": ["in", ["Activo", "Refinanciado"]]
+                                }
+                            };
+                        };
+                    }
+                }
+            });
+        }
+    }
 });
--- erpnext/urbanizaciones/doctype/generar_factura/generar_factura.py
+++ erpnext/urbanizaciones/doctype/generar_factura/generar_factura.py
@@ -1,8 +1,25 @@
 # Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class GenerarFactura(Document):
-	pass
+    pass
+
+@frappe.whitelist()
+def get_financiamientos(customer):
+    """Obtiene los financiamientos activos de un cliente"""
+    if not customer:
+        return []
+    
+    financiamientos = frappe.get_all(
+        "Financiamientos",
+        filters={
+            "customer": customer,
+            "status": ["in", ["Activo", "Refinanciado"]]
+        },
+        fields=["urbanizaciones"]
+    )
+    
+    return financiamientos
